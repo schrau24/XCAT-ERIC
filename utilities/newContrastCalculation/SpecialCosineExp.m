c0a2a3dd375f25e61da3ac60b4abdc8c338e85d1
Schrauben <emschrauben@amc.intra>
2023-06-26 09:55:18 +0200

    new versions of specialCosinefunctions

---
+++ utilities/newContrastCalculation/SpecialCosineExp.m
@@ -0,0 +1,8 @@
+% adapted from Python code:
+% https://github.com/oliverchampion/DCENET
+function f = SpecialCosineExp(x, y)
+    expTerm = (1 - exp(-x)) ./ x;
+
+    trigTerm = (x .* (1 - cos(y))) - (y .* sin(y));
+    f = (trigTerm + (y.^2 .* expTerm)) ./ (x.^2 + y.^2);
+end
---
+++ utilities/newContrastCalculation/SpecialCosineGamma.m
@@ -0,0 +1,9 @@
+% adapted from Python code:
+% https://github.com/oliverchampion/DCENET
+function f = SpecialCosineGamma(x, y)
+    x2 = x.^2;
+    y2 = y.^2;
+    expTerm = (3 + (y2 ./ x2) .* (1 - exp(-x))) - ((y2 + x2) ./ x) .* exp(-x);
+    trigTerm = ((x2 - y2) .* (1 - cos(y))) - (2 * x .* y) .* sin(y);
+    f = (trigTerm + (y2 .* expTerm)) ./ (y2 + x2).^2;
+end
